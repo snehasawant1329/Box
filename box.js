c978cac812d064f1e6012c6eadcb2c0c61f1d823
snehasawant1329 <60028709+snehasawant1329@users.noreply.github.com>
2020-12-03 16:15:15 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,34 @@
+let box = document.createElement("div");
+
+let x = {
+  top: 40,
+  left: 40,
+};
+
+document.addEventListener("keydown", moveBox);
+document.addEventListener("click", newBox);
+
+
+function newBox() {
+  document.body.appendChild(box);
+}
+
+
+
+function moveBox(e) {
+  if (e.keyCode === 37) {
+    x.left -= 10;
+  }
+  if (e.keyCode === 38) {
+    x.top -= 10;
+  }
+  if (e.keyCode === 39) {
+    x.left += 10;
+  }
+  if (e.keyCode === 40) {
+    x.top += 10;
+  }
+
+  box.style.top = x.top + "px";
+  box.style.left = x.left + "px";
+}
